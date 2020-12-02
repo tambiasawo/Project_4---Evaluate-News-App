3d5e829a5e34762c42aeb7241112f592d18d6744
Tambi <tambi.asawo@yahoo.com>
2020-12-02 08:45:30 -0800

    Update scoreDescription.test.js

--- __test__/scoreDescription.test.js
+++ __test__/scoreDescription.test.js
@@ -2,6 +2,6 @@ import { scoreDescription } from '../src/client/js/formHandler'
 
 describe("Testing the score description function", () => {
     test("testing the switch statements functionality", () => {
-        expect(scoreDescription("P")).toBe("POSITIVE")
+        expect(scoreDescription("P")).toBe("Positive")
     });
 });
